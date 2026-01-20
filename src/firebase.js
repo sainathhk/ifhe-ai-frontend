d9b3c0cb0cce5d1a4a9a8e8a44e12709ccb0d205
sainathhk <sainathh.k306@gmail.com>
2026-01-20 09:14:40 +0530

    Prepare frontend for production

--- src/firebase.js
+++ src/firebase.js
@@ -2,9 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBq1TnkR9hT6PBsSrPGBxvkqDn1goraqjU",
-  authDomain: "icfai-ai-assistant.firebaseapp.com",
-  projectId: "icfai-ai-assistant",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
